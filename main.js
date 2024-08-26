7d0a7b446daff824177f34749b85e4ee9dd61a91
thainaramassano <thainara.smassano@gmail.com>
2024-08-25 22:24:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let nome_usuário = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nome_usuário == ""){
+      nome_usuário = prompt("Qual o seu nome?");
+}
+
+if(nome_usuário == null){
+      elemento.textContent = " seja muito bem-vindo!";
+}else{elemento.textContent = nome_usuário;
+
+}
